dcffa46bda60b0d2acaed2288f9cdbc2b599398c
torey-hb <toreymorgan@gmail.com>
2018-03-14 12:00:07 -0700

    V2
    need script js: light gallery and parallax img

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 console.log('Be like Browser.');
 
 // Light Gallery
-$('#gallery-1').lightGallery();
+$('.para').imageScroll({
+  parallax: true,
+  coverRatio: .9,
+});
+
+$('.para2').imageScroll({
+  parallax: true,
+  coverRatio: .5,
+});
+
+$('#gallery-1').lightGallery({
+  mode : 'lg-slide-circular-vertical',
+  download : false,
+  share : false,
+});
